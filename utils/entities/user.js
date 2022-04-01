4c56e933836925c05fe8e9ef0221555da7f220d2
Pavel <pavelgorbatiuk@gmail.com>
2022-04-01 20:41:02 +0300

    New files for new SQL Set

---
+++ utils/entities/user.js
@@ -0,0 +1,146 @@
+import PG from './../pg.js';
+import SqlSetManager from './../sqlSet.js';
+import Kata from './kata.js';
+
+class User {
+  #valid;
+  constructor(tg_id) {
+    this.tg_id = tg_id;
+  }
+
+  //Public methods
+
+  async init() {
+    Object.assign(
+      this,
+      await PG.queryLine('SELECT * FROM users, settings WHERE id = user_id AND tg_id = $1', [
+        this.tg_id,
+      ])
+    );
+    this.#valid = Boolean(this.id);
+  }
+
+  async addKata(kata) {
+    await SqlSetManager.addPair(this.id, kata.id);
+    await kata.updateState();
+  }
+
+  async addKatas(katasArray) {
+    for (const kata of katasArray) {
+      //BOTTLENECK
+      await this.addKata(kata);
+    }
+  }
+
+  async deleteKata(kata) {
+    console.log('start delete kata');
+    await SqlSetManager.deletePair(this.id, kata.id);
+    await kata.updateState();
+    console.log('end delete kata');
+  }
+
+  async deleteKatas(katasArray) {
+    for (const kata of katasArray) {
+      //BOTTLENECK
+      await this.deleteKata(kata);
+    }
+  }
+
+  async toggleSettings(mode) {
+    const client = await PG.getClient();
+
+    try {
+      await client.query('BEGIN');
+
+      const settings = await client.queryLine(
+        `UPDATE settings SET ${mode} = NOT ${mode} WHERE user_id = $1 RETURNING hour, day, month`,
+        [this.id]
+      );
+
+      this[mode] = !this[mode];
+
+      await client.query('COMMIT');
+    } catch (e) {
+      await client.query('ROLLBACK');
+      throw e;
+    } finally {
+      client.release();
+    }
+
+    const usersKatas = await Kata.getKatas((await this.getKataSet()).toArray());
+    await usersKatas.updateState();
+
+    // After remake Kata's settings updateState will not be necessary
+
+    return this.settings;
+  }
+
+  async getKataSet() {
+    return await SqlSetManager.getKataSet(this.id);
+  }
+
+  async getKataCids() {
+    return await SqlSetManager.getUsersKataCids(this.id);
+  }
+
+  async getKataIds() {
+    return await SqlSetManager.getUsersKataIds(this.id);
+  }
+
+  //Predicate methods
+
+  async hasKata(kata) {
+    return await SqlSetManager.hasPair(this.id, kata.id);
+  }
+
+  //Private methods
+
+  async #initArtificially(properties) {
+    Object.assign(this, properties);
+    this.#valid = true;
+  }
+
+  //Getters
+
+  get valid() {
+    return this.#valid;
+  }
+
+  get settings() {
+    const { hour, day, month } = this;
+    return { hour, day, month };
+  }
+
+  //Static methods
+
+  static async createUser(tgId) {
+    const client = await PG.getClient();
+
+    try {
+      await client.query('BEGIN');
+      const userId = await client.queryFirst(`INSERT INTO users (tg_id) VALUES ($1) RETURNING id`, [
+        tgId,
+      ]);
+
+      const properties = await client.queryLine(
+        `INSERT INTO settings (user_id) VALUES ($1) RETURNING *`,
+        [userId]
+      );
+      properties.id = userId;
+
+      const user = new User(tgId);
+      user.#initArtificially(properties);
+
+      await client.query('COMMIT');
+
+      return user;
+    } catch (e) {
+      await client.query('ROLLBACK');
+      throw e;
+    } finally {
+      client.release();
+    }
+  }
+}
+
+export default User;
---
+++ utils/sqlSet.js
@@ -0,0 +1,125 @@
+import PG from './pg.js';
+
+const nameDB = 'subscription';
+
+class SqlSet {
+  #set;
+  #userId;
+  #kataId;
+
+  constructor(typeId, id, iterable) {
+    this.#set = new Set(iterable);
+
+    if (typeId == 'kata') {
+      this.#kataId = id;
+    } else if (typeId == 'user') {
+      this.#userId = id;
+    } else {
+      throw "Type should be 'kata' or 'user'";
+    }
+  }
+
+  async add(value) {
+    this.#set.add(value);
+    await SqlSetManager.addPair(...this.#combine(value));
+  }
+
+  async delete(value) {
+    this.#set.delete(value);
+    await SqlSetManager.deletePair(...this.#combine(value));
+  }
+
+  has(value) {
+    return this.#set.has(value);
+  }
+
+  toArray() {
+    return Array.from(this.#set);
+  }
+
+  get size() {
+    return this.set.size;
+  }
+
+  get id() {
+    return this.#userId ?? this.#kataId;
+  }
+
+  #combine(value) {
+    return [this.#userId ?? value, this.#kataId ?? value];
+  }
+
+  [Symbol.iterator]() {
+    return this.#set[Symbol.iterator].bind(this.#set)();
+  }
+
+  // TODO: create methods:
+  //   - clear()
+}
+
+class SqlUserSet extends SqlSet {
+  constructor(kataId, iterable) {
+    super('kata', kataId, iterable);
+  }
+}
+class SqlKataSet extends SqlSet {
+  constructor(userId, iterable) {
+    super('user', userId, iterable);
+  }
+}
+
+class SqlSetManager {
+  static async getKataSet(userId) {
+    const response = await PG.queryColumn(`SELECT kata_id from ${nameDB} WHERE user_id = $1`, [
+      userId,
+    ]);
+    const kataSet = new SqlKataSet(userId, response);
+    return kataSet;
+  }
+
+  static async getUserSet(kataId) {
+    const response = await PG.queryColumn(`SELECT user_id from ${nameDB} WHERE kata_id = $1`, [
+      kataId,
+    ]);
+    const userSet = new SqlUserSet(kataId, response);
+    return userSet;
+  }
+
+  static async hasPair(userId, kataId) {
+    return PG.queryFirst(`SELECT $1 IN (SELECT kata_id FROM subscription WHERE user_id = $2)`, [
+      kataId,
+      userId,
+    ]);
+  }
+
+  static async addPair(userId, kataId) {
+    await PG.query('INSERT INTO subscription (user_id, kata_id) VALUES ($1, $2)', [userId, kataId]);
+  }
+
+  static async deletePair(userId, kataId) {
+    console.log('start delete pair');
+    await PG.query('DELETE FROM subscription WHERE user_id = $1 AND kata_id = $2', [
+      userId,
+      kataId,
+    ]);
+    console.log('end delete pair');
+  }
+
+  static async getUsersKataCids(userId) {
+    return await PG.queryColumn(
+      `SELECT cid FROM katas WHERE id IN (
+        SELECT kata_id from ${nameDB} WHERE user_id = $1
+      )`,
+      [userId]
+    );
+  }
+  static async getUsersKataIds(userId) {
+    return await PG.queryColumn(`SELECT kata_id from ${nameDB} WHERE user_id = $1`, [userId]);
+  }
+
+  // TODO: create methods:
+  //   - clear()
+  //   - size
+}
+
+export default SqlSetManager;
